29653555692e3169eded7b8e741cb9cc8b85da79
Nathalie Herrera <99284820+NathalieVHerreraP@users.noreply.github.com>
2025-10-14 10:54:18 -0600

    first commit

---
+++ Academy.Documents.WebApi/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Customer API V1");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
